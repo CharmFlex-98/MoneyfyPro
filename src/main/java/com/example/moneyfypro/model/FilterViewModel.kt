570bff4f72ba7aebef4e0c619842dc46e1633149
JiaMing <charmflex1111@gmail.com>
2023-04-19 15:26:24 +0800

    constraint calender so no future date is able to be selected

--- src/main/java/com/example/moneyfypro/model/FilterViewModel.kt
+++ src/main/java/com/example/moneyfypro/model/FilterViewModel.kt
@@ -65,7 +65,7 @@ class FilterViewModel @Inject constructor(private val repository: ExpensesReposi
     /**
      * Get date validator
      */
-    fun dateValidator(startDate: Boolean): CalendarConstraints.DateValidator? {
+    fun dateValidator(startDate: Boolean): DateValidator? {
         if (startDate && _endDate.value != null) return DateValidatorPointBackward.before(_endDate.value!!.time)
         if (!startDate && _startDate.value != null) return CompositeDateValidator.allOf(
             listOf(
@@ -73,30 +73,6 @@ class FilterViewModel @Inject constructor(private val repository: ExpensesReposi
                 DateValidatorPointBackward.before(Date().time)
             )
         )
-        return null
-    }
-
-
-    /**
-     * Get valid start date
-     */
-    fun startDateValidator(): CalendarConstraints.DateValidator? {
-        if (_startDate.value != null) {
-            return DateValidatorPointForward.from(_startDate.value!!.time)
-        }
-
-        return null
-    }
-
-
-    /**
-     * Get valid end date
-     */
-    fun endDateValidator(): CalendarConstraints.DateValidator? {
-        if (_endDate.value != null) {
-            return DateValidatorPointBackward.before(_endDate.value!!.time)
-        }
-
         return null
     }
 
